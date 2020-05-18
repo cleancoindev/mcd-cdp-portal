02fac117cfa176f986a81d05204bb37cbff8dadc
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-05-17 19:44:37 -1000

    Update DSRDeposit.spec.js

--- src/components/__tests__/DSRDeposit.spec.js
+++ src/components/__tests__/DSRDeposit.spec.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import assert from 'assert';
 import { wait, fireEvent, waitForElement } from '@testing-library/react';
-import { MDAI, ETH } from '@makerdao/dai-plugin-mcd';
+import { MTAO, ETH } from '@takertao/tao-plugin-mct';
 import { mineBlocks, TestAccountProvider } from '@makerdao/test-helpers';
 
 import DSRDeposit from '../DSRDeposit';
@@ -9,37 +9,37 @@ import lang from '../../languages';
 import { renderWithAccount } from '../../../test/helpers/render';
 import { instantiateMaker } from '../../maker';
 import { prettifyNumber } from '../../utils/ui';
-import useMaker from 'hooks/useMaker';
+import useTaker from 'hooks/useTaker';
 
 const { click, change } = fireEvent;
 
 const AMOUNT = 80.1234567;
 const ILK = 'ETH-A';
-let maker;
+let taker;
 let web3;
 let noProxyAcct;
 
 beforeAll(async () => {
   // Generate Dai & send to an account with no proxy
-  maker = await instantiateMaker({ network: 'testnet' });
-  await await maker
+  taker = await instantiateTaker({ network: 'testnet' });
+  await await taker
     .service('mcd:cdpManager')
-    .openLockAndDraw(ILK, ETH(1), MDAI(AMOUNT));
+    .openLockAndDraw(ILK, ETH(1), MTAO(AMOUNT));
 
   TestAccountProvider.setIndex(345);
   noProxyAcct = TestAccountProvider.nextAccount();
-  const token = maker.getToken(MDAI.symbol);
+  const token = taker.getToken(MTAO.symbol);
   await token.transfer(noProxyAcct.address, AMOUNT);
 });
 
 const RenderNoProxyAccount = () => {
   const hideOnboarding = jest.fn();
   const [changedAccount, setAccountChanged] = useState(false);
-  const { maker } = useMaker();
-  web3 = maker.service('web3');
+  const { taker } = useTaker();
+  web3 = taker.service('web3');
 
   const changeAccount = async () => {
-    const accountService = maker.service('accounts');
+    const accountService = taker.service('accounts');
     await accountService.addAccount('noproxy', {
       type: 'privateKey',
       key: noProxyAcct.key
@@ -96,7 +96,7 @@ test('the whole DSR Deposit flow', async () => {
   change(input, { target: { value: AMOUNT + 1 } });
   await waitForElement(() =>
     getByText(
-      lang.formatString(lang.action_sidebar.insufficient_balance, 'DAI')
+      lang.formatString(lang.action_sidebar.insufficient_balance, 'TAO')
     )
   );
 
@@ -108,7 +108,7 @@ test('the whole DSR Deposit flow', async () => {
   click(getByText(lang.actions.continue));
 
   getByText(lang.save.deposit_amount);
-  getByText(`${prettifyNumber(AMOUNT)} DAI`);
+  getByText(`${prettifyNumber(AMOUNT)} TAO`);
 
   // Agree to terms to enable deposit button
   click(getByRole('checkbox'));
