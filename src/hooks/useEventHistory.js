0c3da6b4606db4ed6f1690ac2137dc3308ed1ea5
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-05-17 21:39:32 -1000

    Update useEventHistory.js

--- src/hooks/useEventHistory.js
+++ src/hooks/useEventHistory.js
@@ -1,23 +1,23 @@
 import { useEffect, useState } from 'react';
-import useMaker from 'hooks/useMaker';
+import useTaker from 'hooks/useTaker';
 import debug from 'debug';
-const log = debug('maker:useEventHistory');
+const log = debug('taker:useEventHistory');
 
 export default function useEventHistory(id) {
-  const { maker, txLastUpdate } = useMaker();
+  const { taker, txLastUpdate } = useTaker();
   const [events, setEvents] = useState(null);
 
   let isCancelled = false;
   useEffect(() => {
-    if (!maker) return;
+    if (!taker) return;
     async function getHistory() {
       setEvents(null);
       log(`Getting event history for vault #${id}...`);
-      const cdp = await maker
+      const cdp = await taker
         .service('mcd:cdpManager')
         .getCdp(id, { prefetch: false });
       if (isCancelled) return;
-      const events = await maker.service('mcd:cdpManager').getEventHistory(cdp);
+      const events = await taker.service('mcd:cdpManager').getEventHistory(cdp);
       if (isCancelled) return;
       log('Got events for #' + id, events);
       setEvents(events);
@@ -25,7 +25,7 @@ export default function useEventHistory(id) {
     getHistory();
     // eslint-disable-next-line react-hooks/exhaustive-deps
     return () => (isCancelled = true);
-  }, [maker, id, txLastUpdate?.[id]]);
+  }, [taker, id, txLastUpdate?.[id]]);
 
   return events;
 }
