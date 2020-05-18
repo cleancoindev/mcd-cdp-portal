9cea69ca598e3e05294a9de42aa40c5531c4e862
cleancoindev <31389482+cleancoindev@users.noreply.github.com>
2020-05-17 22:04:16 -1000

    Update LanguageProvider.js

--- src/providers/LanguageProvider.js
+++ src/providers/LanguageProvider.js
@@ -2,7 +2,7 @@ import React, { createContext, useState } from 'react';
 import lang, { languages, defaultLocale } from 'languages';
 import { getPreferredLanguage } from 'utils/language';
 import debug from 'debug';
-const log = debug('maker:LanguageProvider');
+const log = debug('taker:LanguageProvider');
 
 export const LanguageContext = createContext();
 
